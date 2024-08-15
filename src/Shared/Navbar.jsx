3f3a3ba9594192d4315c7d78aa3c4b29ad7c1aba
Sahadat Hosen <ssar96540@gmail.com>
2024-08-15 13:42:02 +0600

    complete Logout functionality

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProviders";
 
  
 
 const Navbar = () => {
+  const {user,logOut} = useContext(AuthContext);
+  console.log(user);
+  // Logout
+  const handleLogout = () => {
+    logOut();
+  }
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -40,29 +48,11 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-    <div className="dropdown dropdown-end ">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-     
-    </div>
-  <Link to={'/login'}>  <button className="btn">Login</button></Link>
+   {
+    user? <>
+    <button onClick={handleLogout} className="btn btn-">logOut</button></>:<>
+  <Link to={'/login'}>  <button className="btn">Login</button></Link></>
+   }
   </div>
 </div>
         </div>
