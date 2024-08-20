3dc2b017971277b3f7952309efed2da63fd62254
Sahadat Hosen <ssar96540@gmail.com>
2024-08-20 23:57:22 +0600

    Change Vite_API_URL

--- src/AllCategory.jsx
+++ src/AllCategory.jsx
@@ -13,7 +13,7 @@ const  [categorys,setCategory ] = useState([])
         })
     },[])
 
-console.log(categorys);
+// console.log(categorys);
     return (
         <div className="flex md:flex-col md:justify-around  gap-x-1  md:mr-1 md:m-0 ml-8 mr-8 mt-2 h-full">
           {
